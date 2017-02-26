444b2f55d95249f7b601a3dd87ffcce02de750f2
Bert Van de Casteele <bert@3sign.com>
2017-02-26 17:06:24 +0100

    Added Facade example

---
+++ examples-per-language/php/structural/src/Facade/Computer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace designPatternsForHumans\structural\Facade;
+
+
+class Computer {
+
+  public function getElectricShock() {
+    echo 'Ouch!' . PHP_EOL;
+  }
+
+  public function makeSound() {
+    echo 'Beep, Beep' . PHP_EOL;
+  }
+
+  public function showLoadingScreen() {
+    echo 'Loading ...' . PHP_EOL;
+  }
+
+  public function bam() {
+    echo 'Ready to be used!' . PHP_EOL;
+  }
+
+  public function closeEverything() {
+    echo 'Bup bup bup buzzzzzz' . PHP_EOL;
+  }
+
+  public function pullCurrent() {
+    echo 'Haah!' . PHP_EOL;
+  }
+
+  public function sooth() {
+    echo 'zzzzzzz' . PHP_EOL;
+  }
+
+}
---
+++ examples-per-language/php/structural/src/Facade/ComputerFacade.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace designPatternsForHumans\structural\Facade;
+
+
+class ComputerFacade {
+
+  protected $computer;
+
+  public function __construct(Computer $computer) {
+    $this->computer = $computer;
+  }
+
+  public function turnOn() {
+    $this->computer->getElectricShock();
+    $this->computer->makeSound();
+    $this->computer->showLoadingScreen();
+    $this->computer->bam();
+  }
+
+  public function turnOff() {
+    $this->computer->closeEverything();
+    $this->computer->pullCurrent();
+    $this->computer->sooth();
+  }
+
+
+}
